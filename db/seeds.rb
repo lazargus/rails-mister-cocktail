efe0f06f21db4ebf396eee55007ef4f1d5d49e8d
Augustin Lazarus <augustin.lazarus@free.fr>
2020-05-14 16:40:28 +0200

    beginning of style for index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'json'
+require 'open-uri'
 
 puts "Cleaning database..."
-Ingredient.destroy_all
 
-puts "Creating ingredients..."
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks = JSON.parse(open(url).read)['drinks']
+p drinks
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "Rhum")
-Ingredient.create(name: "Vodka")
-Ingredient.create(name: "Coca")
-Ingredient.create(name: "Tomato Juice")
-
+drinks.each do |drink|
+  # p drink
+  ingredient = Ingredient.create!(name: drink['strIngredient1'])
+  puts "Created #{ingredient.name}"
+end
 
 puts "Finished!"
